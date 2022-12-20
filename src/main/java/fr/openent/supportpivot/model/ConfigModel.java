895d63b28f08bb0d6ec66fe95c01f458a9d03b71
Benard Bastien <bastien.benard@cgi.com>
2022-12-20 11:03:39 +0100

    refactor: rename config field

--- src/main/java/fr/openent/supportpivot/model/ConfigModel.java
+++ src/main/java/fr/openent/supportpivot/model/ConfigModel.java
@@ -45,68 +45,68 @@ public class ConfigModel implements IModel<ConfigModel> {
             log.warn(String.format("[SupportPivot@%s::ConfigModel] Default collectivity absent from configuration", this.getClass().getSimpleName()));
         }
         //We remove "support.demandes" from config
-        this.mongoCollection = config.getString(ConfigField.MONGO_COLLECTION);
+        this.mongoCollection = config.getString(ConfigField.MONGO_DASH_COLLECTION);
         this.academy = config.getString(ConfigField.ACADEMY);
-        this.defaultAttribution = config.getString(ConfigField.DEFAULT_ATTRIBUTION);
-        this.defaultTicketType = config.getString(ConfigField.DEFAULT_TICKETTYPE);
-        this.defaultPriority = config.getString(ConfigField.DEFAULT_PRIORITY);
-        this.jiraLogin = config.getString(ConfigField.JIRA_LOGIN);
-        this.jiraPasswd = config.getString(ConfigField.JIRA_PASSWD);
-        this.jiraHost = config.getString(ConfigField.JIRA_HOST).trim();
-        this.jiraURL = config.getString(ConfigField.JIRA_URL);
-        this.jiraBaseUri = config.getString(ConfigField.JIRA_BASE_URI);
-        this.jiraProjectKey = config.getString(ConfigField.JIRA_PROJECT_KEY);
-        this.jiraAllowedTicketType = config.getJsonArray(ConfigField.JIRA_ALLOWED_TICKETTYPE, new JsonArray()).stream()
+        this.defaultAttribution = config.getString(ConfigField.DEFAULT_DASH_ATTRIBUTION);
+        this.defaultTicketType = config.getString(ConfigField.DEFAULT_DASH_TICKETTYPE);
+        this.defaultPriority = config.getString(ConfigField.DEFAULT_DASH_PRIORITY);
+        this.jiraLogin = config.getString(ConfigField.JIRA_DASH_LOGIN);
+        this.jiraPasswd = config.getString(ConfigField.JIRA_DASH_PASSWD);
+        this.jiraHost = config.getString(ConfigField.JIRA_DASH_HOST).trim();
+        this.jiraURL = config.getString(ConfigField.JIRA_DASH_URL);
+        this.jiraBaseUri = config.getString(ConfigField.JIRA_DASH_BASE_DASH_URI);
+        this.jiraProjectKey = config.getString(ConfigField.JIRA_DASH_PROJECT_DASH_KEY);
+        this.jiraAllowedTicketType = config.getJsonArray(ConfigField.JIRA_DASH_ALLOWED_DASH_TICKETTYPE, new JsonArray()).stream()
                 .filter(o -> o instanceof String)
                 .map(String.class::cast)
                 .collect(Collectors.toList());
-        this.jiraAllowedPriority = config.getJsonArray(ConfigField.JIRA_ALLOWED_PRIORITY, new JsonArray()).stream()
+        this.jiraAllowedPriority = config.getJsonArray(ConfigField.JIRA_DASH_ALLOWED_DASH_PRIORITY, new JsonArray()).stream()
                 .filter(o -> o instanceof String)
                 .map(String.class::cast)
                 .collect(Collectors.toList());
-        this.jiraCustomFields = config.getJsonObject(ConfigField.JIRA_CUSTOM_FIELDS, new JsonObject()).stream()
+        this.jiraCustomFields = config.getJsonObject(ConfigField.JIRA_DASH_CUSTOM_DASH_FIELDS, new JsonObject()).stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, stringObjectEntry -> stringObjectEntry.getValue().toString()));
-        this.jiraStatusConfigMapping = config.getJsonObject(ConfigField.JIRA_STATUS_MAPPING, new JsonObject())
+        this.jiraStatusConfigMapping = config.getJsonObject(ConfigField.JIRA_DASH_STATUS_DASH_MAPPING, new JsonObject())
                 .getJsonObject(Field.STATUTSJIRA, new JsonObject()).stream()
                 .filter(jiraStatusEntry -> jiraStatusEntry.getValue() instanceof JsonArray)
                 .map(jiraStatusEntry -> new JiraStatusConfig(jiraStatusEntry.getKey(), (JsonArray) jiraStatusEntry.getValue()))
                 .collect(Collectors.toList());
-        String defaultJiraStatusString = config.getJsonObject(ConfigField.JIRA_STATUS_MAPPING).getString(Field.STATUTSDEFAULT);
+        String defaultJiraStatusString = config.getJsonObject(ConfigField.JIRA_DASH_STATUS_DASH_MAPPING).getString(Field.STATUTSDEFAULT);
         this.defaultJiraStatusConfig = this.jiraStatusConfigMapping.stream()
                 .filter(jiraStatusConfig -> jiraStatusConfig.getKey().equals(defaultJiraStatusString))
                 .findFirst()
                 .orElse(null);
-        this.entStatusConfigMapping = config.getJsonObject(ConfigField.ENT_STATUS_MAPPING, new JsonObject())
+        this.entStatusConfigMapping = config.getJsonObject(ConfigField.ENT_DASH_STATUS_DASH_MAPPING, new JsonObject())
                 .getJsonObject(EntConstants.STATUTS_ENT, new JsonObject()).stream()
                 .filter(entStatusEntry -> entStatusEntry.getValue() instanceof String)
                 .map(entStatusEntry -> new EntStatusConfig(entStatusEntry.getKey(), (String) entStatusEntry.getValue()))
                 .collect(Collectors.toList());
-        String defaultEntStatusString = config.getJsonObject(ConfigField.ENT_STATUS_MAPPING, new JsonObject()).getString(EntConstants.STATUTSDEFAULTENT);
+        String defaultEntStatusString = config.getJsonObject(ConfigField.ENT_DASH_STATUS_DASH_MAPPING, new JsonObject()).getString(EntConstants.STATUTSDEFAULTENT);
         this.defaultEntStatusConfig = this.entStatusConfigMapping.stream()
                 .filter(entStatusConfig -> entStatusConfig.getName().equals(defaultEntStatusString))
                 .findFirst()
                 .orElse(null);
-        this.proxyHost = config.getString(ConfigField.PROXY_HOST);
-        this.proxyPort = config.getInteger(ConfigField.PROXY_PORT);
+        this.proxyHost = config.getString(ConfigField.PROXY_DASH_HOST);
+        this.proxyPort = config.getInteger(ConfigField.PROXY_DASH_PORT);
     }
 
     @Override
     public JsonObject toJson() {
         JsonObject result = new JsonObject()
                 .put(ConfigField.COLLECTIVITY, this.collectivity)
-                .put(ConfigField.MONGO_COLLECTION, this.mongoCollection)
+                .put(ConfigField.MONGO_DASH_COLLECTION, this.mongoCollection)
                 .put(ConfigField.ACADEMY, this.academy)
-                .put(ConfigField.DEFAULT_ATTRIBUTION, this.defaultAttribution)
-                .put(ConfigField.DEFAULT_TICKETTYPE, this.defaultTicketType)
-                .put(ConfigField.DEFAULT_PRIORITY, this.defaultPriority)
-                .put(ConfigField.JIRA_LOGIN, this.jiraLogin)
-                .put(ConfigField.JIRA_PASSWD, this.jiraPasswd)
-                .put(ConfigField.JIRA_HOST, this.jiraHost)
-                .put(ConfigField.JIRA_URL, this.jiraURL)
-                .put(ConfigField.JIRA_BASE_URI, this.jiraBaseUri)
-                .put(ConfigField.JIRA_PROJECT_KEY, this.jiraProjectKey)
-                .put(ConfigField.JIRA_ALLOWED_TICKETTYPE, new JsonArray(this.jiraAllowedTicketType).copy())
-                .put(ConfigField.JIRA_ALLOWED_PRIORITY, new JsonArray(this.jiraAllowedPriority).copy());
+                .put(ConfigField.DEFAULT_DASH_ATTRIBUTION, this.defaultAttribution)
+                .put(ConfigField.DEFAULT_DASH_TICKETTYPE, this.defaultTicketType)
+                .put(ConfigField.DEFAULT_DASH_PRIORITY, this.defaultPriority)
+                .put(ConfigField.JIRA_DASH_LOGIN, this.jiraLogin)
+                .put(ConfigField.JIRA_DASH_PASSWD, this.jiraPasswd)
+                .put(ConfigField.JIRA_DASH_HOST, this.jiraHost)
+                .put(ConfigField.JIRA_DASH_URL, this.jiraURL)
+                .put(ConfigField.JIRA_DASH_BASE_DASH_URI, this.jiraBaseUri)
+                .put(ConfigField.JIRA_DASH_PROJECT_DASH_KEY, this.jiraProjectKey)
+                .put(ConfigField.JIRA_DASH_ALLOWED_DASH_TICKETTYPE, new JsonArray(this.jiraAllowedTicketType).copy())
+                .put(ConfigField.JIRA_DASH_ALLOWED_DASH_PRIORITY, new JsonArray(this.jiraAllowedPriority).copy());
 
         JsonObject jiraField = new JsonObject();
         this.jiraCustomFields.forEach(jiraField::put);
@@ -123,9 +123,9 @@ public class ConfigModel implements IModel<ConfigModel> {
                 .put(Field.STATUTSENT, entStatus)
                 .put(Field.STATUTSDEFAULTENT, this.defaultEntStatusConfig.getName());
 
-        result.put(ConfigField.JIRA_CUSTOM_FIELDS, jiraField)
-                .put(ConfigField.JIRA_STATUS_MAPPING, jiraMapping)
-                .put(ConfigField.ENT_STATUS_MAPPING, entMapping);
+        result.put(ConfigField.JIRA_DASH_CUSTOM_DASH_FIELDS, jiraField)
+                .put(ConfigField.JIRA_DASH_STATUS_DASH_MAPPING, jiraMapping)
+                .put(ConfigField.ENT_DASH_STATUS_DASH_MAPPING, entMapping);
 
         return result;
     }
