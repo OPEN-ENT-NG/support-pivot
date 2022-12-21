bd140c0887b26e253e30820766c01726fd2edaab
BastienBenard <95362455+BastienBenard@users.noreply.github.com>
2022-12-21 13:02:50 +0100

    refactor: rename config field (#27)
    Co-authored-by: Benard Bastien <bastien.benard@cgi.com>

--- src/main/java/fr/openent/supportpivot/constants/ConfigField.java
+++ src/main/java/fr/openent/supportpivot/constants/ConfigField.java
@@ -1,28 +1,26 @@
 package fr.openent.supportpivot.constants;
 
 public class ConfigField {
-    //Todo remplace JIRA_LOGIN to JIRA_DASH_LOGIN
-    public static final String JIRA_LOGIN = "jira-login";
-    public static final String JIRA_PASSWD = "jira-passwd";
-    public static final String JIRA_HOST = "jira-host";
-    public static final String JIRA_URL = "jira-url";
-    public static final String JIRA_PROJECT_KEY = "jira-project-key";
-    public static final String JIRA_BASE_URI = "jira-base-uri";
+    public static final String JIRA_DASH_LOGIN = "jira-login";
+    public static final String JIRA_DASH_PASSWD = "jira-passwd";
+    public static final String JIRA_DASH_HOST = "jira-host";
+    public static final String JIRA_DASH_URL = "jira-url";
+    public static final String JIRA_DASH_PROJECT_DASH_KEY = "jira-project-key";
+    public static final String JIRA_DASH_BASE_DASH_URI = "jira-base-uri";
     public static final String ACADEMY = "academy";
     public static final String ID_EXTERNAL = "id_external";
     public static final String STATUS_EXTERNAL = "status_external";
     public static final String RESOLUTION_EXTERNAL = "resolution_external";
-    public static final String JIRA_STATUS_MAPPING = "jira-status-mapping";
-    public static final String JIRA_ALLOWED_TICKETTYPE = "jira-allowed-tickettype";
-    public static final String DEFAULT_TICKETTYPE = "default-tickettype";
-    public static final String DEFAULT_PRIORITY = "default-priority";
-    public static final String JIRA_ALLOWED_PRIORITY = "jira-allowed-priority";
-    public static final String JIRA_CUSTOM_FIELDS = "jira-custom-fields";
-    public static final String MAIL_IWS = "mail-iws";
-    public static final String DEFAULT_ATTRIBUTION = "default-attribution";
+    public static final String JIRA_DASH_STATUS_DASH_MAPPING = "jira-status-mapping";
+    public static final String JIRA_DASH_ALLOWED_DASH_TICKETTYPE = "jira-allowed-tickettype";
+    public static final String DEFAULT_DASH_TICKETTYPE = "default-tickettype";
+    public static final String DEFAULT_DASH_PRIORITY = "default-priority";
+    public static final String JIRA_DASH_ALLOWED_DASH_PRIORITY = "jira-allowed-priority";
+    public static final String JIRA_DASH_CUSTOM_DASH_FIELDS = "jira-custom-fields";
+    public static final String DEFAULT_DASH_ATTRIBUTION = "default-attribution";
     public static final String COLLECTIVITY = "collectivity";
-    public static final String MONGO_COLLECTION = "mongo-collection";
-    public static final String PROXY_HOST = "proxy-host";
-    public static final String PROXY_PORT = "proxy-port";
-    public static final String ENT_STATUS_MAPPING = "ent-status-mapping";
+    public static final String MONGO_DASH_COLLECTION = "mongo-collection";
+    public static final String PROXY_DASH_HOST = "proxy-host";
+    public static final String PROXY_DASH_PORT = "proxy-port";
+    public static final String ENT_DASH_STATUS_DASH_MAPPING = "ent-status-mapping";
 }
